ce9e763a721bf93c753b4506792e119d6139b729
leandroheffes24 <leandroheffes@outlook.es>
2024-01-18 16:01:58 -0300

    bars menu interface mobile complete

--- public/js/barsMenu.js
+++ public/js/barsMenu.js
@@ -3,11 +3,13 @@ const menuBarsInterface = document.querySelector(".menuBarsInterface")
 const menuBarsCloseButton = document.querySelector(".headerCloseBarsMenuButton")
 
 menuBarsButton.addEventListener("click", (e) => {
+    console.log("se abrio el menu");
     menuBarsInterface.classList.toggle("display-block")
     menuBarsButton.classList.toggle("display-none")
 })
 
 menuBarsCloseButton.addEventListener("click", (e) => {
+    console.log("se cerro el menu");
     menuBarsInterface.classList.toggle("display-block")
     menuBarsButton.classList.toggle("display-none")
 })
