874a375eb9ec8cd910e2ac6849d26742431b64c2
leandroheffes24 <leandroheffes@outlook.es>
2024-01-19 11:30:13 -0300

    header mobile complete

--- public/js/barsMenu.js
+++ public/js/barsMenu.js
@@ -3,13 +3,11 @@ const menuBarsInterface = document.querySelector(".menuBarsInterface")
 const menuBarsCloseButton = document.querySelector(".headerCloseBarsMenuButton")
 
 menuBarsButton.addEventListener("click", (e) => {
-    console.log("se abrio el menu");
     menuBarsInterface.classList.toggle("display-block")
     menuBarsButton.classList.toggle("display-none")
 })
 
 menuBarsCloseButton.addEventListener("click", (e) => {
-    console.log("se cerro el menu");
     menuBarsInterface.classList.toggle("display-block")
     menuBarsButton.classList.toggle("display-none")
 })
---
+++ public/js/scroll.js
@@ -0,0 +1,14 @@
+let ubicacionPrincipal = window.scrollY
+let header = document.querySelector("header")
+
+window.addEventListener("scroll", function(){
+    let ubicacionActual = window.scrollY
+
+    if(ubicacionPrincipal>= ubicacionActual){
+        header.style.top = "0px"
+    } else {
+        header.style.top = "-135px"
+    }
+
+    ubicacionPrincipal = ubicacionActual
+})
