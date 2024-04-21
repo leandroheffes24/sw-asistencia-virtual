a07d84ddd2b40461ca679c44f8e2512cb207ffa0
Leandro Heffes <leandroheffes@outlook.es>
2024-04-21 14:53:01 -0300

    navbar style corrections

--- public/js/barsMenu.js
+++ public/js/barsMenu.js
@@ -3,11 +3,11 @@ const menuBarsInterface = document.querySelector(".menuBarsInterface")
 const menuBarsCloseButton = document.querySelector(".headerCloseBarsMenuButton")
 
 menuBarsButton.addEventListener("click", (e) => {
-    menuBarsInterface.classList.toggle("display-block")
+    menuBarsInterface.classList.toggle("display-flex")
     menuBarsButton.classList.toggle("display-none")
 })
 
 menuBarsCloseButton.addEventListener("click", (e) => {
-    menuBarsInterface.classList.toggle("display-block")
+    menuBarsInterface.classList.toggle("display-flex")
     menuBarsButton.classList.toggle("display-none")
 })
